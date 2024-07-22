5068ed8ec94e838563439e95d4087e489c659f34
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2024-07-23 00:33:34 +0530

    models created

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
