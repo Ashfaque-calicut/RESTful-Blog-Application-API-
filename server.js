23184451b668ad9437d43366cb205996486abe24
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2024-07-25 20:37:27 +0530

    updated code

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+const postRoutes = require('./routes/postRoutes');
+const commentRoutes = require('./routes/commentRoutes');
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
+  app.use(bodyParser.json());
 
+  app.use('/api', authRoutes);
+  app.use('/api', postRoutes);
+  app.use('/api', commentRoutes);
+  
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500).send({
+      message: err.message,
+      error: err
+    });
+  });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
